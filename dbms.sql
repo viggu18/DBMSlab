bd256926ff94ad70f2b32a580992e99951400a07
viggu18 <nayakvignesh18@gmail.com>
2020-09-05 11:44:46 +0530

    Create dbms.sql

---
+++ dbms.sql
@@ -0,0 +1,6 @@
+create table test3(usn integer primary key, name varchar(10),phone long, branch varchar(10));
+insert into test3 values(100,'viggu',1234567890,'cs');
+insert into test3 values(101,'vikki',956841237,'mech');
+insert into test3 values(102,'pj',856124733,'civil');                                 
+insert into test3 values(103,'lance',15248933,'ec');
+insert into test3 values(104,'gtx',655556555,'is')
